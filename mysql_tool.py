b6c5adb982dbc76ad63e68086527db13cc923f55
bfzq <bigbuggiggle@hotmail.com>
2020-04-03 17:39:17 +0800

    mysql excute

--- mysql_tool.py
+++ mysql_tool.py
@@ -8,7 +8,7 @@ def open_mysql_connection(host, port, user, passwd):
         cnx = mysql.connector.connect(host=host, user=user,
                                     password=passwd, port=port)
     except mysql.connector.Error as err:
-	if err.errno ==	errorcode.ER_ACCESS_DENIED_ERROR:
+	if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your user name or password")
         elif err.errno == errorcode.ER_BAD_DB_ERROR:
 	    print("Database dos not exist")
