ec3d9e6d3d7383753bb5b8c856e87d37888fe796
bfzq <bigbuggiggle@hotmail.com>
2020-05-06 12:00:50 +0800

    fix

--- mysqlc.py
+++ mysqlc.py
@@ -2,59 +2,55 @@
 
 import pymysql as mysql
 
-def open_mysql_connection(host, port, user, passwd):
-    try:
-        cnx = mysql.connect(host=host, user=user, password=passwd, port=port)
-        return cnx
-    except Exception as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database dos not exist")
-        else:
-            print(err)
+def open_mysql_connection(host, port, user, passwd, database):
+  try:
+    cnx = mysql.connect(host=host, user=user, password=passwd, port=port, db=database)
+    return cnx
+  except Exception as err:
+    print(err)
+    exit(1)
 
 def close_mysql_connection(cnx):
-    cnx.close()
+  cnx.close()
 
 def use_database(cnx, DB_NAME):
-    cursor = cnx.cursor()
-    try:
-        cursor.execute("use {}".format(DB_NAME))
-    except Exception as err:
-        print("Database {} does not exists.".format(DB_NAME))
-    cursor.close()
+  cursor = cnx.cursor()
+  try:
+    cursor.execute("use {}".format(DB_NAME))
+  except Exception as err:
+    print(err)
+  cursor.close()
 
 def execute(cnx, sql):
-    cursor = cnx.cursor()
-    try:
-        cursor.execute(sql)
-    except Exception as err:
-        print("{}.SQL : {}".format(err, sql))
-    cursor.close()
+  cursor = cnx.cursor()
+  try:
+    cursor.execute(sql)
+  except Exception as err:
+    print(err)
+  cursor.close()
 
 def last_insert_id(cnx):
-    cursor = cnx.cursor()
-    try:
-        cursor.execute("SELECT LAST_INSERT_ID()")
-        for row in cursor:
-            return row[0]
-    except Exception as err:
-        print("{}.SQL : SELECT LAST_INSERT_ID()".format(err))
-    cursor.close()
-    return -1
+  cursor = cnx.cursor()
+  try:
+    cursor.execute("SELECT LAST_INSERT_ID()")
+    for row in cursor:
+      return row[0]
+  except Exception as err:
+    print(err)
+  cursor.close()
+  return -1
 
 def query(cnx, sql):
-    cursor = cnx.cursor()
-    data = []
-    try:
-        cursor.execute(sql)
-        for row in cursor:
-            data.append(row)
-    except Exception as err:
-        print("{}.SQL : {}".format(err, sql))
-    cursor.close()
-    return data
+  cursor = cnx.cursor()
+  data = []
+  try:
+    cursor.execute(sql)
+    for row in cursor:
+      data.append(row)
+  except Exception as err:
+    print(err)
+  cursor.close()
+  return data
 
 # if __name__ == '__main__':
 #     cnx = open_mysql_connection("127.0.0.1", 3306, "root", "1qaz2wsx")
